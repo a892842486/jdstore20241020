25f81a419d9c07c6fd8a2c41a778f52ea953f572
Bamu <a892842486@github.com>
2024-10-26 14:17:18 +0800

    add OrderMailer function

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -15,6 +15,8 @@ class OrdersController < ApplicationController
         product_list.quantity = cart_item.quantity
         product_list.save
       end
+      current_cart.clean!
+      OrderMailer.notify_order_placed(@order).deliver!
 
       redirect_to order_path(@order.token)
     else
@@ -38,7 +40,7 @@ class OrdersController < ApplicationController
     @order = Order.find_by_token(params[:id])
     @order.set_payment_with!("ewallet")
     @order.pay!
-    redirect_to order_path(@order.token), notice: "使用電子錢包完成付款"    
+    redirect_to order_path(@order.token), notice: "使用電子錢包完成付款"
   end
 
   private
